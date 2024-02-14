4d9213b495e475c46b4ce7929b5967275f49df5f
mkokhaie <linamohammadreza@gmail.com>
2024-02-14 13:39:54 +0330

    train(300ep) added

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 
 
 def yolo8_test():
-    model = YOLO('yolov8n.yml')
-    results = model.track(source="https://youtu.be/LNw0DJXcvt4", show=True)
+    model = YOLO('yolov8n.pt')
+    results = model.train(data='home/mkokhaie/PycharmProjects/Yolo8/datasets/dataset/data.yml', epochs=100, imgsz=640)
     print(results)
 
 
